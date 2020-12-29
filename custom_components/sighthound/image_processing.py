cb994db3c09529f3b8b3f0f6b24d2e57f1c0ab0c
Robin Cole <robmarkcole@gmail.com>
2020-12-29 14:45:30 +0000

    Update image_processing.py

--- custom_components/sighthound/image_processing.py
+++ custom_components/sighthound/image_processing.py
@@ -23,9 +23,11 @@ from homeassistant.util.pil import draw_box
 _LOGGER = logging.getLogger(__name__)
 
 EVENT_PERSON_DETECTED = "sighthound.person_detected"
+EVENT_VEHICLE_DETECTED = "sighthound.vehicle_detected"
 
 ATTR_BOUNDING_BOX = "bounding_box"
 ATTR_PEOPLE = "people"
+ATTR_VEHICLE = "vehicle"
 CONF_ACCOUNT_TYPE = "account_type"
 CONF_SAVE_FILE_FOLDER = "save_file_folder"
 CONF_SAVE_TIMESTAMPTED_FILE = "save_timestamped_file"
@@ -61,19 +63,28 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
 
     entities = []
     for camera in config[CONF_SOURCE]:
-        sighthound = SighthoundEntity(
+        sighthound_person = SighthoundPersonEntity(
             api,
             camera[CONF_ENTITY_ID],
             camera.get(CONF_NAME),
             save_file_folder,
             config[CONF_SAVE_TIMESTAMPTED_FILE],
         )
-        entities.append(sighthound)
+        entities.append(sighthound_person)
+
+        sighthound_vehicle = SighthoundVehicleEntity(
+            api,
+            camera[CONF_ENTITY_ID],
+            camera.get(CONF_NAME),
+            save_file_folder,
+            config[CONF_SAVE_TIMESTAMPTED_FILE],
+        )
+        entities.append(sighthound_vehicle)
     add_entities(entities)
 
 
-class SighthoundEntity(ImageProcessingEntity):
-    """Create a sighthound entity."""
+class SighthoundPersonEntity(ImageProcessingEntity):
+    """Create a sighthound person entity."""
 
     def __init__(
         self, api, camera_entity, name, save_file_folder, save_timestamped_file
@@ -85,7 +96,7 @@ class SighthoundEntity(ImageProcessingEntity):
             self._name = name
         else:
             camera_name = split_entity_id(camera_entity)[1]
-            self._name = f"sighthound_{camera_name}"
+            self._name = f"sighthound_person_{camera_name}"
         self._state = None
         self._last_detection = None
         self._image_width = None
@@ -175,3 +186,102 @@ class SighthoundEntity(ImageProcessingEntity):
         if not self._last_detection:
             return {}
         return {"last_person": self._last_detection}
+
+
+class SighthoundVehicleEntity(ImageProcessingEntity):
+    """Create a sighthound person entity."""
+
+    def __init__(
+        self, api, camera_entity, name, save_file_folder, save_timestamped_file
+    ):
+        """Init."""
+        self._api = api
+        self._camera = camera_entity
+        if name:
+            self._name = name
+        else:
+            camera_name = split_entity_id(camera_entity)[1]
+            self._name = f"sighthound_vehicle_{camera_name}"
+        self._state = None
+        self._last_detection = None
+        self._image_width = None
+        self._image_height = None
+        self._save_file_folder = save_file_folder
+        self._save_timestamped_file = save_timestamped_file
+
+    def process_image(self, image):
+        """Process an image."""
+        detections = self._api.recognize(image, "vehicle,licenseplate")
+        vehicles = hound.get_vehicles(detections)
+        self._state = len(vehicles)
+        if self._state > 0:
+            self._last_detection = dt_util.now().strftime(DATETIME_FORMAT)
+
+        metadata = hound.get_metadata(detections)
+        self._image_width = metadata["image_width"]
+        self._image_height = metadata["image_height"]
+        for vehicle in vehicles:
+            self.fire_vehicle_detected_event(vehicle)
+        if self._save_file_folder and self._state > 0:
+            self.save_image(image, vehicles, self._save_file_folder)
+
+    def fire_vehicle_detected_event(self, vehicle):
+        """Send event."""
+        self.hass.bus.fire(
+            EVENT_VEHICLE_DETECTED, {ATTR_ENTITY_ID: self.entity_id,},
+        )
+
+    def save_image(self, image, vehicles, directory):
+        """Save a timestamped image with bounding boxes around targets."""
+        try:
+            img = Image.open(io.BytesIO(bytearray(image))).convert("RGB")
+        except UnidentifiedImageError:
+            _LOGGER.warning("Sighthound unable to process image, bad data")
+            return
+        draw = ImageDraw.Draw(img)
+
+        for vehicle in vehicles:
+            box = hound.bboxvert_to_tf_style(
+                vehicle["boundingBox"], self._image_width, self._image_height
+            )
+            draw_box(draw, box, self._image_width, self._image_height)
+
+        latest_save_path = directory / f"{self._name}_latest.jpg"
+        img.save(latest_save_path)
+
+        if self._save_timestamped_file:
+            timestamp_save_path = directory / f"{self._name}_{self._last_detection}.jpg"
+            img.save(timestamp_save_path)
+            _LOGGER.info("Sighthound saved file %s", timestamp_save_path)
+
+    @property
+    def camera_entity(self):
+        """Return camera entity id from process pictures."""
+        return self._camera
+
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        return self._name
+
+    @property
+    def should_poll(self):
+        """Return the polling state."""
+        return False
+
+    @property
+    def state(self):
+        """Return the state of the entity."""
+        return self._state
+
+    @property
+    def unit_of_measurement(self):
+        """Return the unit of measurement."""
+        return ATTR_VEHICLE
+
+    @property
+    def device_state_attributes(self):
+        """Return the attributes."""
+        if not self._last_detection:
+            return {}
+        return {"last_vehicle": self._last_detection}
