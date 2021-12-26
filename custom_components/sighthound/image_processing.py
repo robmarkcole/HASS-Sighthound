4874845bfd7b20b0392d2f9bc01870de0cf5f879
Robin Cole <robmarkcole@gmail.com>
2021-12-26 07:54:38 +0000

    Update image_processing.py

--- custom_components/sighthound/image_processing.py
+++ custom_components/sighthound/image_processing.py
@@ -331,7 +331,7 @@ class SighthoundVehicleEntity(ImageProcessingEntity):
         return ATTR_VEHICLE
 
     @property
-    def device_state_attributes(self):
+    def extra_state_attributes(self):
         """Return the attributes."""
         attr = {}
         attr.update({"last_vehicle": self._last_detection})
